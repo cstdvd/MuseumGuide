6833421629b38ed7127d91497818bceac6441f85
Pietro De Rosa <p.derosa0@gmail.com>
2015-05-11 17:08:56 +0200

    Project folder

--- MuseumGuide/app/src/main/java/com/dcpdr/museumguide/MainActivity.java
+++ MuseumGuide/app/src/main/java/com/dcpdr/museumguide/MainActivity.java
@@ -1,39 +1,32 @@
 package com.dcpdr.museumguide;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        // set onClick listener on the button
+        Button button = (Button) findViewById(R.id.enterButton);
+        button.setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onClick(View v)
+    {
+        Intent nextActivityIntent = new Intent(this, MapsActivity.class);
+        startActivity(nextActivityIntent);
     }
 }
