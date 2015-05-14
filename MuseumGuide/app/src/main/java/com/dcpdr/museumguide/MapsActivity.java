039970e11719ef4363efb6b5e0e18eda719ff321
Pietro De Rosa <p.derosa0@gmail.com>
2015-05-14 15:05:58 +0200

    LRU cache library added, MapsActivity updated

--- MuseumGuide/app/src/main/java/com/dcpdr/museumguide/MapsActivity.java
+++ MuseumGuide/app/src/main/java/com/dcpdr/museumguide/MapsActivity.java
@@ -5,13 +5,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.qozix.tileview.TileView;
+
 
 public class MapsActivity extends ActionBarActivity {
+
+    // Constants definition
+    private final int tileHeight = 2512;
+    private final int tileWidth = 2288;
+    private final int tileSize = 128;
+    private final String tilesPath = "tiles/planimetria/";
+    private final String sampleImage = "samples/planimetria/planimetria.jpg";
+    private final String extension = ".jpg";
+    private final float[] detailScales = {1.00f, 0.5f, 0.25f, 0.125f};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+
+        // Create a TileView object
+        TileView tileView = new TileView(this);
+        // Setup the TileView
+        tileView.setSize(tileHeight, tileWidth);
+        for (int i = 0; i < detailScales.length; i++)
+            tileView.addDetailLevel(detailScales[i], tilesPath + detailScales[i] * 100 + "/%col%_%row%" + extension, sampleImage, tileSize, tileSize);
+        // Define the bounds using the map size in pixel
+        tileView.defineRelativeBounds(0, 0, tileHeight, tileWidth);
+        // Display the TileView
+        tileView.setScale(0);
+        setContentView(tileView);
     }
 
 
